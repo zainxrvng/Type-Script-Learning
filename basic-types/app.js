1d1c27ead269a345917d352b0b44d63144932fbb
zainxrvng <muhammadzainkhan29@gmail.com>
2026-06-02 22:40:54 +0500

    learning basic types

---
+++ basic-types/app.js
@@ -0,0 +1,15 @@
+"use strict";
+// Primative and Referance
+Object.defineProperty(exports, "__esModule", { value: true });
+// the difference is this 
+let a = "zain";
+let b = a;
+//
+let c = [1, 2, 3, 4, 5, 6];
+let d = c.pop();
+console.log(c);
+//
+//tuples
+let arr = ["zain", 20];
+// enumerations
+//# sourceMappingURL=app.js.map
---
+++ basic-types/app.ts
@@ -0,0 +1,20 @@
+// Primative and Referance
+
+// the difference is this 
+
+let a = "zain";
+let b = a
+
+//
+
+let c = [1,2,3,4,5,6]
+let d = c.pop()
+console.log(c);
+
+//
+//tuples
+
+let arr: [string, number] = ["zain", 20]
+
+// enumerations
+
