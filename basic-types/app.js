5c6299b8fa3051180c56c5ef4bffc7ae1556693d
zainxrvng <muhammadzainkhan29@gmail.com>
2026-06-04 00:18:49 +0500

    learned enums, any, unkown, void, null, never

--- basic-types/app.js
+++ basic-types/app.js
@@ -1,7 +1,7 @@
 "use strict";
 // Primative and Referance
 Object.defineProperty(exports, "__esModule", { value: true });
-// the difference is this 
+// the difference is this
 let a = "zain";
 let b = a;
 //
@@ -12,4 +12,28 @@ console.log(c);
 //tuples
 let arr = ["zain", 20];
 // enumerations
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["Admin"] = "zain";
+    UserRoles["Guest"] = "guest";
+    UserRoles["Super_Admin"] = "admin";
+})(UserRoles || (UserRoles = {}));
+console.log(UserRoles.Admin);
+// any
+let t; // this is any
+let r;
+r = 15;
+r = "zain";
+// unknown it make u check the type later to perfrom specific funtions
+let q;
+// void
+function zain() {
+    console.log("zain");
+}
+// never
+function typeNever() {
+    while (true) { }
+}
+typeNever();
+console.log("hello");
 //# sourceMappingURL=app.js.map
--- basic-types/app.ts
+++ basic-types/app.ts
@@ -1,20 +1,55 @@
 // Primative and Referance
 
-// the difference is this 
+// the difference is this
 
 let a = "zain";
-let b = a
+let b = a;
 
 //
 
-let c = [1,2,3,4,5,6]
-let d = c.pop()
+let c = [1, 2, 3, 4, 5, 6];
+let d = c.pop();
 console.log(c);
 
 //
 //tuples
 
-let arr: [string, number] = ["zain", 20]
+let arr: [string, number] = ["zain", 20];
 
 // enumerations
+
+enum UserRoles {
+  Admin = "zain",
+  Guest = "guest",
+  Super_Admin = "admin",
+}
+
+console.log(UserRoles.Admin);
+
+// any
+let t; // this is any
+let r: number;
+
+r = 15;
+r = "zain";
+
+// unknown it make u check the type later to perfrom specific funtions
+
+let q:unknown;
+
+
+// void
+
+function zain (): void {
+  console.log("zain")
+}
+
+// never
+
+function typeNever () :never{
+  while (true) {}
+}
+
+typeNever()
+console.log("hello");
 
