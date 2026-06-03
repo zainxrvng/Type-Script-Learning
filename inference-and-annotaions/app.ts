27162d8563e3fae32d54a5619141768476401c3b
zainxrvng <muhammadzainkhan29@gmail.com>
2026-06-04 00:27:38 +0500

    learning inferance and annotations

---
+++ inference-and-annotaions/app.ts
@@ -0,0 +1,17 @@
+// inference
+
+let a = 12;
+let b = "zain";
+let c = true;
+
+// basicly it picks the type on its own we dont have to define it
+
+// Annotations
+
+let d: string = "zain";
+let e: number = 13;
+let f: boolean = false;
+
+// in this we have to manullay set up the types.
+
+function hello(a: string, b: number) {}
