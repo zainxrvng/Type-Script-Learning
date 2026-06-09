df8eb99edb17879ad8f4b5d7086c4cbefeb85a7d
zainxrvng <muhammadzainkhan29@gmail.com>
2026-06-09 19:28:04 +0500

    learning constructors

--- Classes-and-objects/app.ts
+++ Classes-and-objects/app.ts
@@ -8,3 +8,29 @@ class Device {
 
 let d1 = new Device()
 let d2 = new Device()
+
+
+/// constructor
+
+/// this is the short way to wrote construstor
+class BottleMaker {
+  constructor(public name: string, public price: number) { }
+}
+
+new BottleMaker("zain", 1200)
+
+// this is the long way to write the constructor
+class Music {
+  public name;
+  public artist;
+  public thumbnail;
+  public length;
+  public free;
+  constructor(name:string, artist:string, thumbnail:string = "somethumbnail.jpg", length:number, free:boolean ) {
+    this.name = name
+    this.artist = artist
+    this.thumbnail = thumbnail
+    this.length = length
+    this.free = free
+  }
+}
