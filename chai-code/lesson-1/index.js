85643cb284370e74fc9cba1fb2ec507de041fe39
zainxrvng <muhammadzainkhan29@gmail.com>
2026-08-25 02:52:36 +0500

    Add initial TypeScript files with greet function and configuration settings

---
+++ chai-code/lesson-1/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello ${name}`;
+}
+const username = "lora garlic";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ chai-code/lesson-2/index.ts
@@ -0,0 +1,9 @@
+/// annotation and inference
+
+// annotation means i will teach and tell you
+
+
+
+
+
+// inference means you will learn by your self
